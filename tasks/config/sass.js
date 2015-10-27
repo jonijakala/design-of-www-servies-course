cbafb941ec83090e541e670ffbe9578b4e1853d5
chriisu <chriisu@gmail.com>
2015-10-27 13:19:37 +0200

    bootstrap variables
    Added the bootstrap variables to styles ready to be configured.

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -1,14 +1,14 @@
 /**
- * Compiles LESS files into CSS.
+ * Compiles SASS files into CSS.
  *
  * ---------------------------------------------------------------
  *
- * Only the `assets/styles/importer.less` is compiled.
+ * Only the `assets/styles/importer.scss` is compiled.
  * This allows you to control the ordering yourself, i.e. import your
  * dependencies, mixins, variables, resets, etc. before other stylesheets)
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-less
+ * 		https://github.com/gruntjs/grunt-contrib-sass
  */
 module.exports = function(grunt) {
 
@@ -18,18 +18,10 @@ module.exports = function(grunt) {
      			style: 'expanded', //Set your prefered style for development here.
      			loadPath: 'node_modules/bootstrap-sass/assets/stylesheets/'
     		},
-	    	files: [
-	    	// {
-	     //  		expand: true,
-	     //  		cwd: 'node_modules/bootstrap-sass/assets/stylesheets/',
-	     //  		src: ['*.scss'], // Feel free to remove a format if you do not use it.
-	     //  		dest: '.tmp/public/styles/',
-	     //  		ext: '.css'
-	    	// },
-	    	{
+	    	files: [{
 	      		expand: true,
 	      		cwd: 'assets/styles/',
-	      		src: ['importer.scss'], // Feel free to remove a format if you do not use it.
+	      		src: ['importer.scss'],
 	      		dest: '.tmp/public/styles/',
 	      		ext: '.css'
 	    	}]
@@ -38,45 +30,3 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 };
-
-
-
-// module.exports = function(grunt) {
-
-// 	grunt.config.set('less', {
-// 		dev: {
-// 			files: [{
-// 				expand: true,
-// 				cwd: 'assets/styles/',
-// 				src: ['importer.less'],
-// 				dest: '.tmp/public/styles/',
-// 				ext: '.css'
-// 			}]
-// 		}
-// 	});
-
-// 	grunt.loadNpmTasks('grunt-contrib-less');
-// };
-
-
-// sass: {
-//   dev: {
-//     options: {
-//       style: 'expanded' //Set your prefered style for development here.
-//     }
-//     files: [{
-//       expand: true,
-//       cwd: 'assets/styles/',
-//       src: ['*.scss', '*.sass'], // Feel free to remove a format if you do not use it.
-//       dest: '.tmp/public/styles/',
-//       ext: '.css'
-//     }, {
-//       expand: true,
-//       cwd: 'assets/linker/styles/',
-//       src: ['*.scss', '*.sass'], // Feel free to remove a format if you do not use it.
-//       dest: '.tmp/public/linker/styles/',
-//       ext: '.css'
-//     }
-//     ]
-//   }
-// },
