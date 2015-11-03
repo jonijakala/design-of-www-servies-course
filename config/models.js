ec9cf83f7c646229e6e8bffc96eae9f97f4ce80c
chriisu <chriisu@gmail.com>
2015-11-03 14:39:13 +0200

    MongoBranch

--- config/models.js
+++ config/models.js
@@ -18,7 +18,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
   // connection: 'localDiskDb',
-
+  connection: 'sailsMongodbServer',
   /***************************************************************************
   *                                                                          *
   * How and whether Sails will attempt to automatically rebuild the          *
