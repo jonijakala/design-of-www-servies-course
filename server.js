749410cae74d958fcde8387e2ed0c9eaaca998d3
chriisu <chriisu@gmail.com>
2016-01-05 15:28:36 +0200

    dummyapi & gulp

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
 
 // configuration ===========================================
-	
+
 // config files
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+mongoose.connect('mongodb://webres:asdf@ds037395.mongolab.com:37395/webres');
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+// app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+// var router = require('./app/routes')(app);
+app.use('/api', require('./app/apiRoutes')());
+app.use('/', require('./app/angularRoutes'));
+
+// Error Handling
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+// });
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// app.use('/api', router);
+// require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
+app.listen(port);
+console.log('Magic happens on port ' + port); // shoutout to the user
+exports = module.exports = app; // expose app
